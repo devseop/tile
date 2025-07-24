3ae42660b42a3f60ded078366ff0b92861c7d896
devseop <cocc263@gmail.com>
2025-07-24 17:45:19 +0900

    :art: 세부정보를 테이블로

--- src/components/WeatherInfoContainer.tsx
+++ src/components/WeatherInfoContainer.tsx
@@ -45,9 +45,65 @@ export const WeatherInfoContainer = () => {
             <span className="text-[17px] text-[#CACACA]">{district}</span>
           </p>
         </div>
+        {/* //! 데이터 업데이트 필요 */}
         {/* 세부정보 */}
-        <div className='flex'>
-          <span>세부 정보</span>
+        <div className="overflow-x-auto">
+          <table className="w-full divide-y divide-[#D9D9D9] border-collapse text-sm">
+            <thead>
+              <tr>
+                <th colSpan={4} className="py-2 text-[#878787] text-left font-normal">
+                  세부정보
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-[#D9D9D9]">
+              {/* 최고/최저 */}
+              <tr>
+                <td colSpan={4}>
+                  <div className="flex gap-6">
+                    {/* 좌측 50% */}
+                    <div className="flex-1 flex items-center justify-between">
+                      <span className="font-medium">최고</span>
+                      <span className="py-2">25°</span>
+                    </div>
+                    {/* 우측 50% */}
+                    <div className="flex-1 flex items-center justify-between">
+                      <span className="font-medium">최저</span>
+                      <span className="py-2">12°</span>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              {/* 체감온도 */}
+              <tr className='justify-between'>
+                <td className="py-2 font-medium">체감온도</td>
+                <td className="py-2" />
+                <td className="py-2" />
+                <td className="py-2 text-right">25°</td>
+              </tr>
+              {/* 미세먼지 */}
+              <tr>
+                <td className="py-2 font-medium">미세먼지</td>
+                <td className="py-2" />
+                <td className="py-2" />
+                <td className="py-2 text-right">좋음</td>
+              </tr>
+              {/* 초미세먼지 */}
+              <tr>
+                <td className="py-2 font-medium">초미세먼지</td>
+                <td className="py-2" />
+                <td className="py-2" />
+                <td className="py-2 text-right">좋음</td>
+              </tr>
+              {/* 자외선 */}
+              <tr>
+                <td className="py-2 font-medium">자외선</td>
+                <td className="py-2" />
+                <td className="py-2" />
+                <td className="py-2 text-right">좋음</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </section>
   )
